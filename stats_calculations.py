18e4136da46c70e0f0cc9254c25610c3cd256190
Quocviet Le <quocvietlet@gmail.com>
2024-08-01 23:11:34 -0500

    quick select algo and stdev rounding

--- stats_calculations.py
+++ stats_calculations.py
@@ -1,57 +1,75 @@
 """ This module implements simple statistics functions. """
 
 from math import sqrt
+from collections import Counter
+import statistics
 
 
 def getNumbers():
     """This function gets numbers from a user and returns it in a list."""
-    nums = []  # start with an empty list
+    while True:
+        try:
+            numbers = input("Enter a list of numbers separated by spaces: ")
+            num_list = [float(num) for num in numbers.split()]
+            if not num_list:
+                print("Please enter a valid list of numbers.")
+            return num_list
+        except ValueError as e:
+            print("Please enter a valid list of numbers.")
 
-    # sentinel loop to get numbers
-    xStr = input("Enter a number (<Enter> to quit) >> ")
-    while xStr != "":
-        x = float(xStr)
-        nums.append(x)  # add this value to the list
-        xStr = input("Enter a number (<Enter> to quit) >> ")
-    return nums
 
-
-def mean(nums):
+def mean(numbers):
     """This function takes a list of numbers as a parameter
     and returns the mean."""
-    sum = 0.0  # initialize the sum to 0
-    for num in nums:
-        sum = sum + num  # increment the sum
-    return sum / len(nums)
+    return sum(numbers) / len(numbers)
 
 
-def stdDev(nums, xbar):
+def stdDev(numbers, xbar):
     """This function takes a list of numbers and a mean as parameters
     and returns the sample standard deviation."""
     sumDevSq = 0.0  # initialize the sum of deviation squares to 0
-    for num in nums:
+    for num in numbers:
         dev = xbar - num  # deviation from mean
         sumDevSq = sumDevSq + dev * dev  # increment
-    return sqrt(sumDevSq / (len(nums) - 1))
+    return round(sqrt(sumDevSq / (len(numbers) - 1)), 3)
 
 
-def median(nums):
+def quick_select(numbers, k):
+    """This function takes a list of numbers and an index k as parameters
+    and returns the kth smallest element in the list."""
+    if len(numbers) == 1:
+        return numbers[0]
+    pivot = numbers[len(numbers) // 2]
+    less = [i for i in numbers if i < pivot]  # all elements less than the pivot
+    greater = [i for i in numbers if i > pivot]  # 3 all elements greater than the pivot
+    equal = [i for i in numbers if i == pivot]  # all elements equal to the pivot
+    if k < len(less):
+        return quick_select(less, k)
+    elif k < len(less) + len(equal):
+        return equal[0]
+    else:
+        return quick_select(greater, k - len(less) - len(equal))
+
+
+def median(numbers):
     """This function takes a list of numbers as a paremeter
     and returns the median."""
-    nums.sort()  # sort the numbers
-    size = len(nums)
-    midPos = size // 2  # middle position (by integer division by 2)
-    if size % 2 == 0:  # even
-        median = (nums[midPos] + nums[midPos - 1]) / 2
-    else:  # odd
-        median = nums[midPos]
-    return median
+    size = len(numbers)
+    if size == 0:
+        raise ValueError("list is empty, median requires at least one data point")
+
+    if size % 2 == 1:
+        return quick_select(numbers, size // 2)
+    else:
+        return (
+            quick_select(numbers, size // 2 - 1) + quick_select(numbers, size // 2) / 2
+        )
 
 
-def geometricMean(nums):
+def geometricMean(numbers):
     m = 1
-    n = len(nums)
-    for i in nums:
+    n = len(numbers)
+    for i in numbers:
         m = m * i
     geometric_mean = m ** (1 / n)
     print("Geometric Mean is: " + str(geometric_mean))
--- stats_user.py
+++ stats_user.py
@@ -1,14 +1,15 @@
 """ This program uses the stats.py statistics module. """
 
-import stats_calculations
+import stats_calculations as stats
+from stats_calculations import getNumbers, mean, stdDev, median
 
 
 def main():
     print("This program computes mean, median and standard deviation.")
-    data = stats_calculations.getNumbers()
-    xbar = stats_calculations.mean(data)
-    std = stats_calculations.stdDev(data, xbar)
-    med = stats_calculations.median(data)
+    data = stats.getNumbers()
+    xbar = stats.mean(data)
+    std = stats.stdDev(data, xbar)
+    med = stats.median(data)
     print("\nThe mean is", xbar)
     print("The standard deviation is", std)
     print("The median is", med)
